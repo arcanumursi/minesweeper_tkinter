b88322cf6ef586e422f7f76ddd9d76a5f6854397
Bjørnar Borge <72221999+arcanumursi@users.noreply.github.com>
2022-04-21 15:00:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from cell import Cell
 import settings
 import utilities
 
@@ -11,7 +12,7 @@ root.resizable(False, False)
 
 top_frame = Frame(
     root, 
-    bg='red', # Change later to black
+    bg='black',
     width=settings.WIDTH,
     height=utilities.height_percentage(25)
 )
@@ -19,11 +20,34 @@ top_frame.place(x=0, y=0)
 
 left_frame = Frame(
     root, 
-    bg='blue', # Change later to black
-    width=utilities.height_percentage(25),
+    bg='black', 
+    width=utilities.width_percentage(25),
     height=utilities.height_percentage(75)
 )
-
 left_frame.place(x=0, y=utilities.height_percentage(25))
+
+center_frame = Frame(
+    root,
+    bg='black',  
+    width=utilities.width_percentage(75),
+    height=utilities.height_percentage(75)
+)
+center_frame.place(
+    x=utilities.width_percentage(25), 
+    y=utilities.height_percentage(25)
+)
+
+# Create cells
+for x in range(settings.GRID_SIZE):
+    for y in range(settings.GRID_SIZE):
+        c = Cell(x, y)
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(
+            column=x,
+            row=y
+        )
+
+Cell.randomize_mines()
+
 # Run the window
 root.mainloop()
