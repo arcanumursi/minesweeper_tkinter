2594ef57f474e56b316e3fc8d7ba783a02dc75ae
Bjørnar Borge <72221999+arcanumursi@users.noreply.github.com>
2022-04-24 10:40:41 +0200

    Update cell.py

--- cell.py
+++ cell.py
@@ -1,11 +1,14 @@
-from tkinter import Button
+from tkinter import Button, Label
 import random
 import settings
 
 class Cell:
     all = []
+    cell_count = settings.CELL_COUNT
+    cell_count_label_object = None
     def __init__(self, x, y, is_mine=False):
         self.is_mine = is_mine
+        self.is_opened = False
         self.cell_btn_object = None
         self.x = x
         self.y = y
@@ -18,16 +21,29 @@ class Cell:
             location,
             width=12,
             height=4,
-            text=f"{self.x},{self.y}"
         )
         btn.bind('<Button-1>', self.left_blick_actions) # Left click
         btn.bind('<Button-3>', self.right_blick_actions) # Right click
         self.cell_btn_object = btn
 
+    @staticmethod
+    def create_cell_count_label(location):
+        lbl = Label(
+            location,
+            bg='black',
+            fg='white',
+            text=f"Cells Left:{Cell.cell_count}",
+            font=("", 30)
+        )
+        Cell.cell_count_label_object = lbl
+
     def left_blick_actions(self, event):
         if self.is_mine:
             self.show_mine()
         else:
+            if self.surrounded_cells_mines_length == 0:
+                for cell_obj in self.surrounded_cells:
+                    cell_obj.show_cell()
             self.show_cell()
     
     def get_cell_by_axis(self, x, y):
@@ -36,8 +52,40 @@ class Cell:
             if cell.x == x and cell.y == y:
                 return cell
 
+    @property
+    def surrounded_cells(self):
+        cells = [
+            self.get_cell_by_axis(self.x - 1, self.y - 1),
+            self.get_cell_by_axis(self.x - 1, self.y),
+            self.get_cell_by_axis(self.x - 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y),
+            self.get_cell_by_axis(self.x + 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y + 1)
+        ]
+        cells = [cell for cell in cells if cell is not None]
+        return cells
+    
+    @property
+    def surrounded_cells_mines_length(self):
+        counter = 0
+        for cell in self.surrounded_cells:
+            if cell.is_mine:
+                counter += 1
+        return counter
+
     def show_cell(self):
-        print(self.get_cell_by_axis(0, 0))
+        if not self.is_opened:
+            Cell.cell_count -= 1
+            print(self.cell_btn_object.configure(text=self.surrounded_cells_mines_length))
+            # Replace the text of cell count label with the never count
+            if Cell.cell_count_label_object:
+                Cell.cell_count_label_object.configure(
+                    text=f"Cells Left:{Cell.cell_count}"
+                )
+        # Mark the cell as opened (Use it as the last line of this method)
+        self.is_opened = True
 
     def show_mine(self):
         # A logic to interrupt the game and display a message that player lost!
